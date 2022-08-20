bb9d57364d78ce827626d5466111a508be594da2
nafiz <nafiz.2011@yahoo.com>
2022-08-20 22:09:24 +0600

    add url

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,7 @@ use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\View;
+use \Illuminate\Support\Facades\URL;
 
 
 use Illuminate\Support\ServiceProvider;
