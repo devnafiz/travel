ed7274e4e7cdf06d54b40a103fce1e6e3f456495
nafiz <nafiz.2011@yahoo.com>
2022-08-20 22:19:25 +0600

    add meta

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -5,6 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width,initial-scale=1">
     <link rel="icon" href="images/favicon.png" type="image/x-icon">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     
     <!-- Google Fonts -->	
     <link href="https://fonts.googleapis.com/css?family=Lato:300,300i,400,400i,700,700i,900,900i%7CMerriweather:300,300i,400,400i,700,700i,900,900i" rel="stylesheet">
